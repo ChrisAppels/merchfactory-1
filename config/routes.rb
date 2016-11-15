24ebba1afe16739d3970f65483eba2adc02aefbf
Daniel Bleakley <daniel.bleakley@merchfactory.com>
2016-11-15 13:41:49 +0100

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-root to: "home#index"
+  root to: "home#index"
+  resources :products only: [:index, :show]
+
+  resources :orders only: [:create, :show, :checkout, :payment, :confirmation, :index] do
+    resources :order_items only: [:create ]
+  end
+  resources :users only: [:update, :edit]
+
+  resources :product_designs only: [:index]
 
 end
-
