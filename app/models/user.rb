783448ac11eff4dd52c9cff8ab7686f820aeeb37
Daniel Bleakley <daniel.bleakley@merchfactory.com>
2016-11-15 18:02:02 +0100

    add db seed

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :user_roles
+  has_many :roles, through: :user_roles
   has_many :orders
   has_many :addresses
   has_many :product_designs
