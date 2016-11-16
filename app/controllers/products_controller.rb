9d343fae59830fbfc840b178b69e046772dae187
Alexandre <alexedron@gmail.com>
2016-11-16 11:56:54 +0100

    nav

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,23 @@
 class ProductsController < ApplicationController
+before_action :authenticate_user!, except: [:index, :show]
+before_action :find_product, only: [:show]
+
   def index
+    if params[:query] == "products"
+    return @products = Product.all
+  elsif params[:query] && params[:query].length>0
+    @products = Product.query(params[:query])
+  else
+    @products = Product.all
+  end
   end
 
   def show
   end
+end
+
+private
+
+def find_product
+    @product = Product.find(params[:id])
 end
