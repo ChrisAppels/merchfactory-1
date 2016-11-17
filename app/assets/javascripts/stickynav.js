41aad4d4af668c4629bdc4eba1ff1d984be1008f
Alexandre <alexedron@gmail.com>
2016-11-17 15:43:16 +0100

    Adds a new and improved navbar

--- app/assets/javascripts/stickynav.js
+++ app/assets/javascripts/stickynav.js
@@ -1,10 +1,12 @@
 $(document).ready(function(){
 
   function stickyNav(ev){
-    if(window.pageYOffset>70){
-      $('#bottom-nav').addClass('sticky-nav');
+    if(window.pageYOffset>101){
+      $('.bottom-nav').addClass('sticky-nav');
+      $('#nav-logo').addClass('show-inline');
     } else {
-      $('#bottom-nav').removeClass('sticky-nav');
+      $('.bottom-nav').removeClass('sticky-nav');
+      $('#nav-logo').removeClass('show-inline');
     }
   }
   window.onscroll=stickyNav
