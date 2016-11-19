74f34e5d21eae59660feaaceea81df3f66b0030a
Daniel Bleakley <daniel.bleakley@merchfactory.com>
2016-11-19 19:40:40 +0100

    update user role in DB. Remove user-roles table. Connect user to role table directly

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161118102532) do
+ActiveRecord::Schema.define(version: 20161119183001) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -166,15 +166,6 @@ ActiveRecord::Schema.define(version: 20161118102532) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "user_roles", force: :cascade do |t|
-    t.integer  "user_id"
-    t.integer  "role_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["role_id"], name: "index_user_roles_on_role_id", using: :btree
-    t.index ["user_id"], name: "index_user_roles_on_user_id", using: :btree
-  end
-
   create_table "users", force: :cascade do |t|
     t.string   "email"
     t.string   "encrypted_password",     default: "",    null: false
@@ -190,11 +181,11 @@ ActiveRecord::Schema.define(version: 20161118102532) do
     t.string   "last_name"
     t.string   "phone"
     t.string   "company_name"
-    t.string   "role"
     t.string   "avatar"
     t.datetime "created_at",                             null: false
     t.datetime "updated_at",                             null: false
     t.boolean  "admin",                  default: false, null: false
+    t.integer  "role_id"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
@@ -214,6 +205,4 @@ ActiveRecord::Schema.define(version: 20161118102532) do
   add_foreign_key "product_designs", "products"
   add_foreign_key "product_designs", "users"
   add_foreign_key "product_templates", "products"
-  add_foreign_key "user_roles", "roles"
-  add_foreign_key "user_roles", "users"
 end
