9b271e7baeb801c69cf47dc5a5204bf83a7613f1
Chris Appels <appelscj@gmail.com>
2016-11-23 16:05:56 +0100

    oops masterino

--- db/seeds.rb
+++ db/seeds.rb
@@ -303,7 +303,7 @@ end
 
 
 address_params = [{
-  user_id: User.find_by(first_name: 'Pieter'),
+  user_id: (User.find_by(first_name: 'Pieter')).id,
   unit_number: "5",
   street_number: "28",
   street_name: "Down",
@@ -313,7 +313,7 @@ address_params = [{
   country: "Australia"
 },
 {
-  user_id: User.find_by(first_name: 'Pieter'),
+  user_id: (User.find_by(first_name: 'Pieter')).id,
   unit_number: "3",
   street_number: "420",
   street_name: "Herengracht",
