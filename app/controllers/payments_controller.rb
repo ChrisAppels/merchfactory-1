cdda1c07202b270039be1f34861534077593e6d4
Chris Appels <appelscj@gmail.com>
2016-11-23 14:13:12 +0100

    sorry lonneke

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -13,21 +13,20 @@ def create
   charge = Stripe::Charge.create(
     customer:     customer.id,   # You should store this customer id and re-use it.
     amount:       @order.amount_cents, # or amount_pennies
-    description:  "dickbut",
+    description:  "MERCH-Factory Custom Merchandise",
     currency:     :eur
   )
 
   @order.update(payment: charge.to_json, status: 'paid')
-  redirect_to order_path(@order)
+  redirect_to orders_checkout_path
 
 rescue Stripe::CardError => e
   flash[:error] = e.message
-  redirect_to new_order_payment_path(@order)
+  redirect_to orders_checkout_path
 end
   private
 
   def set_order
     @order = current_user.orders.last
   end
-
 end
--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -2,6 +2,7 @@ class OrderItem < ApplicationRecord
   belongs_to :product
   belongs_to :orderable, polymorphic: true
   has_many :order_item_designs
+  validates :quantity, numericality: { greater_than: 0 }, presence: true
 
 def totalprice
   (self.quantity * self.product.item_price_for_quantity(self.quantity).price_cents) / 100.0
