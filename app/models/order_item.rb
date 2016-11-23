6a634f841914b6f857195c258835d770b6dd99c2
Daniel Bleakley <daniel.bleakley@merchfactory.com>
2016-11-23 14:37:53 +0100

    style Shopping Cart

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -2,8 +2,9 @@ class OrderItem < ApplicationRecord
   belongs_to :product
   belongs_to :orderable, polymorphic: true
   has_many :order_item_designs
+  validates :quantity, :numericality => { :greater_than => 0 }
 
 def totalprice
-  (self.quantity * self.product.item_price_for_quantity(self.quantity).price_cents) / 100.0
+  (self.quantity * self.product.item_price_for_quantity(self.quantity).price_cents) / 100.00
 end
 end
