e56138670bafc8d92f039f68e333a650e080acf0
Chris Appels <appelscj@gmail.com>
2016-11-24 16:39:28 +0100

    login route

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,14 @@ class ApplicationController < ActionController::Base
   # before_action :session_cart
   helper_method :session_cart
 
+  def after_sign_in_path_for(resource)
+    if session_cart #your path
+      shopping_cart_path(current_user.id)
+    else
+      root_path
+    end
+  end
+
   def session_cart
     if session[:shopping_cart_id]
       begin
