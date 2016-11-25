9f315e9de18ab0f9798741489d2e4cb624b2f75a
Daniel Bleakley <daniel.bleakley@merchfactory.com>
2016-11-25 12:40:00 +0100

    add to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -448,7 +448,7 @@ end
 
 
 address_params = [{
-  user_id: (User.find_by(first_name: 'Pieter')).id,
+  user_id: (User.find_by(first_name: 'Chris')).id,
   unit_number: "5",
   street_number: "28",
   street_name: "Down",
@@ -458,7 +458,7 @@ address_params = [{
   country: "Australia"
 },
 {
-  user_id: (User.find_by(first_name: 'Pieter')).id,
+  user_id: (User.find_by(first_name: 'Chris')).id,
   unit_number: "3",
   street_number: "420",
   street_name: "Herengracht",
